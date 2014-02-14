9fd07eac06f7e586bc161c5fa48e85a15b7aeea3
usuario <usuario@debian.gonzalonazareno.org>
2014-02-14 21:33:57 +0100

    Terminado los scripts

--- baja.py
+++ baja.py
@@ -22,3 +22,22 @@ else:
     basereload = "FLUSH PRIVILEGES;"
     tabla.execute(basereload)
     db.commit()
+#PROFTPD
+    borrarcolumna="delete from usuarios where dominio='%s';" % (dominio)
+    tabla.execute(borrarcolumna)
+    basereload = "FLUSH PRIVILEGES;"
+    tabla.execute(basereload)
+    db.commit()
+
+#CARPETAS PERSONALES Y DE APACHE2
+    os.system("rm -r /srv/www/%s" % resultado)
+    os.system("rm -r /home/%s" % resultado)
+    os.system("a2dissite %s" % resultado)
+    os.system("a2dissite phpmyadmin%s" % resultado)
+    os.system("rm -r /etc/apache2/sites-available/%s" % resultado)
+    os.system("service apache2 restart")
+    os.system("rm -r /etc/apache2/sites-available/phpmyadmin%s" % resultado)
+    os.system("rm -r /var/cache/bind/db.%s" % dominio)
+    os.system("sed '/zone " + '"%s"'% dominio + "/,/};/d' /etc/bind/named.conf.local > temporal")
+    os.system("mv temporal /etc/bind/named.conf.local")
+    print "El usuario %s con dominio %s se elimino correctamente" % (resultado,dominio)
--- change_password.py
+++ change_password.py
@@ -13,3 +13,22 @@ tabla2 = db2.cursor()
 consulta = "select username from usuarios where username='%s';" % (usuario)
 tabla.execute(consulta)
 resultado = tabla.fetchone()
+if resultado == None:
+    print "El usuario %s no exite" % (usuario)
+else:
+    if "-sql" == sys.argv[2]:
+        print "La nueva contraseña para MySQL del usuario %s es %s" % (usuario,dominio)
+        consulta2 = "update user set password=PASSWORD('%s') where user='%s';" % (dominio,usuario)
+        tabla2.execute(consulta2)
+        basereload = "FLUSH PRIVILEGES;"
+        tabla2.execute(basereload)
+        db2.commit()
+
+    elif "-ftp" == sys.argv[2]:
+        print "La nueva contraseña para Proftpd del usuario %s es %s" % (usuario,dominio)
+        consulta3 = "update usuarios set password=PASSWORD('%s') where username='%s';" % (dominio,usuario)
+        tabla.execute(consulta3)
+        basereload = "FLUSH PRIVILEGES;"
+        tabla.execute(basereload)
+        db.commit()
+
